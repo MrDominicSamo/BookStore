f939c4dbfb295ceb0a8eeb952b3f1f5521c40baf
MrDominicSamo <dominic.samo@res.co.sz>
2026-08-14 12:20:35 +0200

    Virtual fields

--- srv/service.js
+++ srv/service.js
@@ -2,7 +2,7 @@ const cds = require("@sap/cds");
 
 module.exports = class BookStoreService extends cds.ApplicationService {
   init() {
-    const { Books, Genre } = cds.entities;
+    const { Books, Genre, Authors } = cds.entities;
 
     this.on("addDiscount", async () => {
       await UPDATE(Books).set({
@@ -35,22 +35,40 @@ module.exports = class BookStoreService extends cds.ApplicationService {
       await UPDATE(Books).set({ status_code: newStatus }).where({ ID: bookId });
     });
 
-    this.before("READ", Books, async (req) => {
+    this.before("READ", "Books", async (req) => {
       console.log("Before READ Books");
     });
 
-    this.on("READ", Books, async (req, next) => {
+    this.on("READ", "Books", async (req, next) => {
       console.log("ON EVENT");
       return next();
     });
 
-    this.after("READ", Books, async (books, req) => {
+    this.after("READ", "Books", async (books, req) => {
       for (const book of books) {
         if (book.genre_code === Genre.Art) {
           book.price = book.price * 0.8;
         }
       }
       console.log("After READ Books");
+    });
+
+    this.after("READ", "Authors", async (authors) => {
+      const ids = authors.map((author) => author.ID);
+
+      console.log("Author IDs:", ids);
+
+      const bookCounts = await SELECT.from(Books)
+        .columns("author_ID", { func: "count" })
+        .where({ author_ID: { in: ids } })
+        .groupBy("author_ID");
+
+      for (const author of authors) {
+        const count = bookCounts.find((bc) => bc.author_ID === author.ID);
+        author.bookCount = count ? count.count : 0;
+      }
+
+      console.log("Book Counts:", bookCounts);
     });
 
     return super.init();
